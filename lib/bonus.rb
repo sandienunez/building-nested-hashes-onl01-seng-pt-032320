544b42f0267bd74dcdc046399a4a009272dba18b
Sandie Nunez <snunez@vt.edu>
2020-04-05 18:46:33 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,10 +23,6 @@ puts epic_tragedy
       ]
    }
 }
-  #code your solution here:
-
-  
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
+  end
 end
